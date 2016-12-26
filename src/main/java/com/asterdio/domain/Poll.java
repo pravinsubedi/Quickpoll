a8b27c11174f58bb0e633c54c15b37524370aab8
pravinsubedi <mepravinsubedi@gmail.com>
2016-12-26 17:45:00 +0545

    Initial Commit

--- src/main/java/com/asterdio/domain/Poll.java
+++ src/main/java/com/asterdio/domain/Poll.java
@@ -1,7 +1,19 @@
 package com.asterdio.domain;
 
+import javax.persistence.*;
+
 /**
  * Created by Admin on 12/26/2016.
  */
+@Entity
 public class Poll {
+
+    @GeneratedValue
+    @Column(name = "POLL_ID")
+    private long poll_id;
+    @Column(name = "QUESTION")
+    private String question;
+
+    @OneToMany()
+    @JoinColumn(name = "POLL_ID")
 }
